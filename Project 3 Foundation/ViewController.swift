a673eed6b5c313b9d301a601fb1df28f04a2ad4b
Steve Liddle <liddle@byu.edu>
2015-10-28 08:50:20 -0600

    Initial Commit

---
+++ Project 3 Foundation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 3 Foundation
+//
+//  Created by Steve Liddle on 10/28/15.
+//  Copyright © 2015 Steve Liddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
